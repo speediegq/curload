dc7b04bb9c2d1a37d9a0c0975f21d29fb36aa1fe
speedie <speedie@speedie.site>
2023-09-28 23:22:40 +0200

    Add config.ini #5

--- config.php
+++ config.php
@@ -1,8 +1,23 @@
 <?php
-    $Stylesheet  = "index.css"; // path to stylesheet
-    $Icon        = "favicon.svg"; // path to favicon
-    $uploadDir   = "uploads/"; // directory to store uploads in
-    $keyFile     = "passwords.txt"; // text file containing primary passwords
-    $tempKeyFile = "temporary_passwords.txt"; // text file containing temporary passwords
-    $maxFileSize = "100"; // in megabytes
+    $Stylesheet  = "index.css";
+    $Icon        = "favicon.svg";
+    $uploadDir   = "uploads/";
+    $keyFile     = "passwords.txt";
+    $tempKeyFile = "temporary_passwords.txt";
+    $maxFileSize = "100";
+
+    define('CONFIG_FILE', 'config.ini');
+
+    if (!file_exists(CONFIG_FILE)) {
+        return;
+    }
+
+    /* load config file */
+    $configEntries = parse_ini_file(CONFIG_FILE);
+    $Stylesheet = $configEntries['css'];
+    $Icon = $configEntries['favicon'];
+    $uploadDir = $configEntries['upload_dir'];
+    $keyFile = $configEntries['key_file'];
+    $tempKeyFile = $configEntries['temp_key_file'];
+    $maxFileSize = $configEntries['max_size'];
 ?>
